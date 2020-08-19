acb0e082e74eb69715a66d62306610784f560010
yonlu <lss.emailbox@gmail.com>
2020-08-18 21:03:23 -0400

    feat: isolating components

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Logon</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+
+        <Button type="submit">Enter</Button>
+
+        <a href="forgot">Forgot your password?</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Create an account
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default SignIn;
