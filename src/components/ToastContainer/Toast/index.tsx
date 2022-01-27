5d0f347848deec9747d2b998a5a31291c13fe9bd
yonlu <lss.emailbox@gmail.com>
2022-01-27 17:11:23 -0500

    refactor: change toast timer to 6000ms

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -27,7 +27,7 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       removeToast(message.id);
-    }, 3000);
+    }, 6000);
 
     return () => {
       clearTimeout(timer);
